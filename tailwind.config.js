be29b47710f4dcf8bb68a23d44200e05c35e66a7
velvethouse <velvethouse@velvethouseagency.com>
2025-08-31 20:00:41 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        velvet: "#0b0b0c",
+        gold: "#C5A880",
+        rose: "#e6c6d8"
+      }
+    }
+  },
+  plugins: []
+};
