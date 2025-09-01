e2abc38016ccfdd5cd6a5d1a35e311c31073e309
velvethouse <velvethouse@velvethouseagency.com>
2025-09-01 18:37:47 +0200

    Create head.tsx

---
+++ app/contact/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Contact | Velvet House Agency</title>
+      <meta name="description" content="Écris-nous pour toute question (partenariats, VIP, créatrices, agences)." />
+    </>
+  );
+}
