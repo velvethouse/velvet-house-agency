b6dd6a4c37a66e9caa562dd9f4cd2b8fbe9343f5
velvethouse <velvethouse@velvethouseagency.com>
2025-09-01 18:40:09 +0200

    Create head.tsx

---
+++ app/nova/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Nova | Velvet House Agency</title>
+      <meta name="description" content="Nova, l’assistante intégrée de Velvet House Agency." />
+    </>
+  );
+}
