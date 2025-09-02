1201a32dc538615b7f65cda153963856e1bdd056
velvethouse <velvethouse@velvethouseagency.com>
2025-09-02 20:22:47 +0200

    Create head.tsx

---
+++ app/live/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Live | Velvet House Agency</title>
+      <meta
+        name="description"
+        content="Browse upcoming & current live sessions by country and language."
+      />
+    </>
+  );
+}
