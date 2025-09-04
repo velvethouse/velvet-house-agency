b30c4ba39ea344530e6f425ef1644b4300afa093
velvethouse <velvethouse@velvethouseagency.com>
2025-09-04 06:56:21 +0200

    Create head.tsx

---
+++ app/vip/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>VIP | Velvet House Agency</title>
+      <meta name="description" content="Become VIP: full non-NSFW galleries, priority access, exclusive rewards. NSFW remains gift-unlock only." />
+    </>
+  );
+}
