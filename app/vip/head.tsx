fd46d54f16f247928c50aa2ebbfbf7dcbeb32d3c
velvethouse <velvethouse@velvethouseagency.com>
2025-09-04 07:27:32 +0200

    Delete app/vip/head.tsx

--- app/vip/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>VIP | Velvet House Agency</title>
-      <meta name="description" content="Become VIP: full non-NSFW galleries, priority access, exclusive rewards. NSFW remains gift-unlock only." />
-    </>
-  );
-}
