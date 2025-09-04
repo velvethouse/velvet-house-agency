53e35298d774b053fcd18020618b247deb92e08e
velvethouse <velvethouse@velvethouseagency.com>
2025-09-04 07:26:29 +0200

    Delete app/nova/head.tsx

--- app/nova/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Nova | Velvet House Agency</title>
-      <meta name="description" content="Nova, l’assistante intégrée de Velvet House Agency." />
-    </>
-  );
-}
