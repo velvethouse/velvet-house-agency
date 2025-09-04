1556f5e52c2565e5e470ee81b0b76f0678e5d4a9
velvethouse <velvethouse@velvethouseagency.com>
2025-09-04 07:25:32 +0200

    Delete app/contact/head.tsx

--- app/contact/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Contact | Velvet House Agency</title>
-      <meta name="description" content="Écris-nous pour toute question (partenariats, VIP, créatrices, agences)." />
-    </>
-  );
-}
