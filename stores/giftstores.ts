522444df6ca4d5bf31ef8fe8e49121f81c945bd2
velvethouse <velvethouse@velvethouseagency.com>
2025-09-05 19:43:06 +0200

    Create giftstores.ts

---
+++ stores/giftstores.ts
@@ -0,0 +1,28 @@
+"use client";
+import { create } from "zustand";
+
+export type Gift = {
+  id: string;
+  name: string;
+  kind: "lottie" | "static";
+  src: string;          // /gifts/xxx.json ou .png
+  durationMs?: number;  // ex: 2000
+};
+
+type GiftState = {
+  queue: Gift[];
+  push: (g: Gift) => void;
+  shift: () => Gift | undefined;
+  clear: () => void;
+};
+
+export const useGiftStore = create<GiftState>((set, get) => ({
+  queue: [],
+  push: (g) => set((s) => ({ queue: [...s.queue, g] })),
+  shift: () => {
+    const [head, ...rest] = get().queue;
+    set({ queue: rest });
+    return head;
+  },
+  clear: () => set({ queue: [] }),
+}));
