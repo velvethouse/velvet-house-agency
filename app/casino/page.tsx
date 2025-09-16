685b28d9b0fec725eb26b1aedf902f052d96043d
velvethouse <velvethouse@velvethouseagency.com>
2025-09-16 19:05:09 +0200

    Update page.tsx

--- app/casino/page.tsx
+++ app/casino/page.tsx
@@ -1,18 +1,16 @@
-import CasinoIntro from './components/CasinoIntro';
 import JackpotDisplay from './components/JackpotDisplay';
-import CasinoExplanation from './components/CasinoExplanation';
 import GamesGrid from './components/GamesGrid';
-import CasinoDisclaimer from './components/CasinoDisclaimer';
+import JackpotTicker from './components/JackpotTicker';
+import RulesBookButton from './components/RulesBookButton';
 
 export default function CasinoPage() {
   return (
-    <main className="min-h-screen bg-black text-white px-4 py-8 space-y-10">
-      <div className="max-w-5xl mx-auto space-y-10">
-        <CasinoIntro />
+    <main className="min-h-screen bg-black text-white px-4 py-8">
+      <div className="max-w-5xl mx-auto space-y-6 relative">
         <JackpotDisplay />
-        <CasinoExplanation />
+        <JackpotTicker />
         <GamesGrid />
-        <CasinoDisclaimer />
+        <RulesBookButton />
       </div>
     </main>
   );
